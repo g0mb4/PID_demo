bb0b042c18e5d2f19cedd1ed332106e638ac6e41
g0mb4 <gomba007@gmail.com>
2022-04-08 09:49:20 +0200

    Implement better Anti-Windup.

--- PID_demo/pid_controller.cpp
+++ PID_demo/pid_controller.cpp
@@ -2,32 +2,38 @@
 
 #include "pid_controller.h"
 
-static float P = 0, I = 0, D = 0;
+static float I = 0;
 
-static void AntiWindup(const ProgramState * state){
+static float AntiWindup(const ProgramState * state, float control_signal, float dI){
 	/*
 		Only run if anti-windup is enabled.
 	*/
 	if(!state->anti_windup){
-		return;
+		return control_signal;
 	}
 	
 	/*
-		Reset 'I' if it would cause saturation.
+		Neglect integrator if output saturates.
 	*/
-	if(I > OUT_MAX || I < OUT_MIN){
-		I = 0;
+	if(control_signal > OUT_MAX || control_signal < OUT_MIN){
+	    I -= dI;
+		control_signal -= dI;
 	}
+	
+	return control_signal;
 }
 
 float UpdateController(const ProgramState * state, float delta_time) {
-	P = state->k_P * state->control_error;
-	I += state->k_I  * state->control_error * delta_time;
-	D = state->k_D * (state->dcontrol_error / delta_time);
-
-	AntiWindup(state);
-
+	float P = state->k_P * state->control_error;
+	float dI = state->k_I  * state->control_error * delta_time;
+	float D = state->k_D * (state->dcontrol_error / delta_time);
+	
+    I += dI;
+    
 	float control_signal = P + D + I;
+	
+	control_signal = AntiWindup(state, control_signal, dI);
+	
 	control_signal = constrain(control_signal, OUT_MIN, OUT_MAX);
 
 	return control_signal;
